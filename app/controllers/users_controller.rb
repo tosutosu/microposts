6a54a2e0b40f9a43b504a8a85fd54bba230d35cc
chidatoshihiro <toshi.c.1986@gmail.com>
2015-08-16 04:50:41 +0000

    challenge1. New Futuer. add user area. Add eddting page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,21 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: '編集しました'
+    else
+      render 'edit'
+    end
+  end
+  
 private
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :area)
   end
 end
