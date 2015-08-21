b0117bd150b9e03adafc9352da29f0765c02aa63
chidatoshihiro <toshi.c.1986@gmail.com>
2015-08-21 08:51:01 +0000

    Fix MigrationFiles

--- db/migrate/20150816151012_create_microposts.rb
+++ db/migrate/20150816151012_create_microposts.rb
@@ -1,11 +1,11 @@
 class CreateMicroposts < ActiveRecord::Migration
   def change
     create_table :microposts do |t|
-      t.references :user, index: true, foreign_key: true
+      t.references :user, index: true
       t.text :content
 
-      t.timestamps null: false
-      t.index [:follower, :followed], unique: true
+      t.timestamps
+      t.index [:user_id, :created_at]
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,11 @@ ActiveRecord::Schema.define(version: 20150817110053) do
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
+  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
   add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
 
   create_table "relationships", force: :cascade do |t|
@@ -40,5 +41,7 @@ ActiveRecord::Schema.define(version: 20150817110053) do
     t.datetime "updated_at",      null: false
     t.string   "area"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
