234017a2461dbc0a16aa75853800b1de49182ee5
chidatoshihiro <toshi.c.1986@gmail.com>
2015-08-23 04:16:26 +0000

    Fix. Image post

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,16 +1,17 @@
 class MicropostsController < ApplicationController
     before_action :logged_in_user, only: [:create]
-    
     def create
         @micropost = current_user.microposts.build(micropost_params)
         if @micropost.save
             flash[:success] = "Micropost created!"
             redirect_to root_url
         else
+            # Original
+            @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
             render 'static_pages/home'
-       end
+        end
     end
-    
+
     def destroy
        @micropost = current_user.microposts.find_by(id: params[:id])
        return redirect_to root_url if @micropost.nil?
